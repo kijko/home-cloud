c72ba01e6fccc9d82a55df65ff42c1d89c393bd4
kijko <kijkomaciej@gmail.com>
2021-02-13 18:39:49 +0100

    initialize client react app

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const App = () => <div>App component edited !</div>;
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const DevServerConfig = require("./webpack-dev-server.config");
+
+module.exports = {
+    "mode": "development",
+    "entry": "./src/index.js",
+    "output": {
+        "path": __dirname + '/dist',
+        "filename": "bundle.js"
+    },
+    "devtool": "source-map",
+    "module": {
+        "rules": [
+            {
+                "enforce": "pre",
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "loader": "eslint-loader",
+                "options": {
+                    "emitWarning": true,
+                    "failOnError": false,
+                    "failOnWarning": false
+                }
+            },
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader"
+                }
+            },
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                "test": /\.html$/,
+                "use": [
+                    {
+                        "loader": "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    "plugins": [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ],
+    "devServer": DevServerConfig,
+};
